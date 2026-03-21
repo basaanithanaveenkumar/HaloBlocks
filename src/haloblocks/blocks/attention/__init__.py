0b38c8c84f187069838609e17b7068261c295808
naveen <basaanithanaveenkumar1998@gmail.com>
2026-03-21 12:56:21 +0530

    release v0.1.2

--- src/haloblocks/blocks/attention/__init__.py
+++ src/haloblocks/blocks/attention/__init__.py
@@ -4,5 +4,6 @@ from . import trinity_attention
 from . import cross_attention
 from . import mqa
 from . import gqa
+from . import multi_head_latent_attention
 
-__all__ = ["self_attention", "scaledotprod", "trinity_attention", "cross_attention", "mqa", "gqa"]
+__all__ = ["self_attention", "scaledotprod", "trinity_attention", "cross_attention", "mqa", "gqa", "multi_head_latent_attention"]
